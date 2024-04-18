36b14efd25b1db7371bb4f9f1b4c6fe5b9e62a3e
CodeShft <nileceyazilim@gmail.com>
2024-04-19 01:01:52 +0300

    weather app project was created.

---
+++ src/App.jsx
@@ -0,0 +1,124 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import Card from "./components/Card";
+import videoBg from "./video/videoBg.mp4";
+import Rain from "./components/Rain";
+
+
+function App() {
+  const [coords, setCoords] = useState([]);
+  const [locationName, setLocationName] = useState(null);
+  const [currentWeather, setCurrentWeather] = useState();
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+
+  const API_KEY = "78d8ff879664f60bbc5fe7d5392e503c";
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  const getLocation = () => {
+    if (!navigator.geolocation) {
+    } else {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const lat = position.coords.latitude;
+        const long = position.coords.longitude;
+        setCoords([lat, long]);
+        const obj = {
+          lat: lat,
+          long: long,
+        };
+        searchLocation(obj, "latlong");
+      });
+    }
+  };
+
+  const searchLocation = async (query, type = "search") => {
+    let API_URL = `https://api.openweathermap.org/data/2.5/forecast?q=${query}&appid=${API_KEY}&units=metric&lang=tr`;
+    if (type === "latlong") {
+      API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${query.lat}&lon=${query.long}&appid=${API_KEY}&units=metric&lang=tr`;
+    }
+    try {
+      const response = await axios.get(API_URL).then((response) => {
+        let dataTemp = [];
+        //Günü ayarlama
+        for (let i = 0; i < 6; i++) {
+          dataTemp.push(response.data.list[i * 1]);
+        }
+
+        setData(response.data);
+        setLocationName(response.data.city.name);
+        setLocation(response.data.city.name);
+        setCurrentWeather(dataTemp);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleClick = () => {
+    window.location.href = "./components/Rain";
+  };
+ <Rain/>
+
+  return (
+    <div className="main">
+      <div className="overlay"></div>
+      <video autoPlay loop muted>
+        <source src={videoBg} type="video/mp4" />
+      </video>
+      <div className="content">
+        <div className="navbar">
+          <div className="none gap-1">
+            <button
+              className="btn btn-ghost btn-circle avatar"
+              
+            > 
+              <div className="w-12 rounded-full">
+                <img
+                  alt=""
+                  src="https://i.pinimg.com/736x/02/32/ce/0232cec1a50de0b489b0591b64451204.jpg" onClick={handleClick} 
+                />
+              </div>
+            </button>
+            <h1 className="weather1">Weather App</h1>
+
+            <div className="form-control">
+              <input
+                onClick={getLocation}
+                type="text"
+                placeholder="Enter location"
+                className="input input-bordered w-24 md:w-auto "
+                value={location}
+                onChange={(event) => setLocation(event.target.value)}
+                onKeyDownCapture={(event) => {
+                  if (event.key === "Enter") {
+                    searchLocation(location);
+                  }
+                }}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="h3 rounded-xl shadow-md shadow-gray-500 ">
+          <h1 className="h1">{locationName}</h1>
+          {currentWeather && <Card currentWeather={currentWeather} />}
+        </div>
+        <footer className="footer">
+          <p className="p1">-Made by Nil Ece Altuğ © 2024 -</p>
+          <img
+            className="gif"
+            alt=""
+            src="https://cdn.dribbble.com/users/2120934/screenshots/6193512/11_rain.gif"
+          />
+          <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end"></nav>
+        </footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+
+function convertEpoch(value) {
+  if (!value) {
+    return "";
+  }
+  const time = new Date(Number(value * 1000));
+  if (isNaN(time.valueOf())) {
+    return "";
+  }
+  // 2. Türkçe dil seçenekleri
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    hour12: false,
+    timeZone: "Europe/Istanbul", // Ankara için zaman dilimini ayarlama
+  };
+
+  // 3. Date objesini formatlama
+  const formattedDate = time.toLocaleDateString("tr-TR", options);
+  return formattedDate;
+}
+
+const Card = ({ currentWeather }) => {
+  return (
+    <div className="flex justify-center items-center">
+      <div className="card card-compact w-95 bg-base-100  shadow-md shadow-gray-500 my-30 mx-auto">
+        <div className="card-body">
+          <table className="table table-sm rounded-sm">
+            <thead className="thead">
+              <tr>
+                <th className="text-bold ">Tarih</th>
+                <th className="text-bold">Hava Durumu</th>
+                <th className="text-bold">Sıcaklık</th>
+                <th className="text-bold">Nem</th>
+                <th className="text-bold">Rüzgar</th>
+              </tr>
+            </thead>
+            <tbody>
+              {currentWeather.map((item, index) => (
+                <tr key={index}>
+                  <td className="text-bold">{convertEpoch(item.dt, "date")}</td>
+                  <td className="flex items-center">
+                    <img
+                      src={`https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`}
+                      alt={item.weather[0].description}
+                      className="w-8 h-8 mr-2"
+                    />
+                    <span className="text-bold">
+                      {item.weather[0].description}
+                    </span>
+                  </td>
+                  <td className="text-bold">
+                    {item.main.temp} derece (hissedilen: {item.main.feels_like})
+                  </td>
+                  <td className="text-bold">{item.main.humidity}</td>
+                  <td className="text-bold">{item.wind.speed}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
