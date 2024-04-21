f205f774f2b9c165a2aafb88e699a1075f1a8812
CodeShft <nileceyazilim@gmail.com>
2024-04-21 08:19:12 +0300

    last edit.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,69 +1,68 @@
 import PropTypes from "prop-types";
 
 function convertEpoch(value) {
-	if (!value) {
-		return "";
-	}
-	const time = new Date(Number(value * 1000));
-	if (Number.isNaN(time.valueOf())) {
-		return "";
-	}
-	// 2. Türkçe dil seçenekleri
-	const options = {
-		weekday: "long",
-		year: "numeric",
-		month: "long",
-		day: "numeric",
-		hour: "numeric",
-		minute: "numeric",
-		hour12: false,
-		timeZone: "Europe/Istanbul", // Ankara için zaman dilimini ayarlama
-	};
+  if (!value) {
+    return "";
+  }
+  const time = new Date(Number(value * 1000));
+  if (Number.isNaN(time.valueOf())) {
+    return "";
+  }
+  // 2. Türkçe dil seçenekleri
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    hour12: false,
+    timeZone: "Europe/Istanbul", // Ankara için zaman dilimini ayarlama
+  };
 
-	// 3. Date objesini formatlama
-	const formattedDate = time.toLocaleDateString("tr-TR", options);
-	return formattedDate;
+  // 3. Date objesini formatlama
+  const formattedDate = time.toLocaleDateString("tr-TR", options);
+  return formattedDate;
 }
 
-export default function Card({currentWeather}) {
+export default function Card({ currentWeather }) {
   return (
-        <div className="overflow-x-auto">
-          <table className="table">
-            <thead className="thead">
-              <tr>
-                <th>Tarih</th>
-                <th>Hava Durumu</th>
-                <th>Sıcaklık</th>
-                <th>Nem</th>
-                <th>Rüzgar</th>
-              </tr>
-            </thead>
-            <tbody>
-              {currentWeather.map((item) => (
-                <tr key={item.dt}>
-                  <td className="text-bold">{convertEpoch(item.dt, "date")}</td>
-                  <td className="flex items-center">
-                    <img
-                      src={`https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`}
-                      alt={item.weather[0].description}
-                      className="w-8 h-8 mr-2"
-                    />
-                    <span className="text-bold">
-                      {item.weather[0].description}
-                    </span>
-                  </td>
-                  <td className="text-bold">
-                    {item.main.temp.toFixed(1)} C° (hissedilen: {item.main.feels_like.toFixed(1)} C°)
-                  </td>
-                  <td className="text-bold">{item.main.humidity}</td>
-                  <td className="text-bold">{item.wind.speed}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+    <div className="overflow-x-auto">
+      <table className="table">
+        <thead className="thead">
+          <tr>
+            <th>Tarih</th>
+            <th>Hava Durumu</th>
+            <th>Sıcaklık</th>
+            <th>Nem</th>
+            <th>Rüzgar</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentWeather.map((item) => (
+            <tr key={item.dt}>
+              <td className="text-bold">{convertEpoch(item.dt, "date")}</td>
+              <td className="flex items-center">
+                <img
+                  src={`https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`}
+                  alt={item.weather[0].description}
+                  className="w-8 h-8 mr-2"
+                />
+                <span className="text-bold">{item.weather[0].description}</span>
+              </td>
+              <td className="text-bold">
+                {item.main.temp.toFixed(1)} C° (hissedilen:{" "}
+                {item.main.feels_like.toFixed(1)} C°)
+              </td>
+              <td className="text-bold">{item.main.humidity}</td>
+              <td className="text-bold">{item.wind.speed}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 Card.propTypes = {
   currentWeather: PropTypes.arrayOf(PropTypes.object).isRequired,
-}
+};
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -6,92 +6,94 @@ import Background from "./Background";
 import Card from "./Card";
 
 function HomePage() {
-	const [locationName, setLocationName] = useState(null);
-	const [currentWeather, setCurrentWeather] = useState();
-	const [location, setLocation] = useState("");
-	const [weatherId, setWeatherId] = useState(null);
+  const [locationName, setLocationName] = useState(null);
+  const [currentWeather, setCurrentWeather] = useState();
+  const [location, setLocation] = useState("");
+  const [weatherId, setWeatherId] = useState(null);
 
-	const API_KEY = "78d8ff879664f60bbc5fe7d5392e503c";
+  const API_KEY = "78d8ff879664f60bbc5fe7d5392e503c";
 
-	useEffect(() => {
-		getLocation();
-	});
+  useEffect(() => {
+    getLocation();
+  });
 
-	const getLocation = () => {
-		if (navigator.geolocation) {
-			navigator.geolocation.getCurrentPosition((position) => {
-				const lat = position.coords.latitude;
-				const long = position.coords.longitude;
-				const obj = {
-					lat: lat,
-					long: long,
-				};
-				searchLocation(obj, "latlong");
-			});
-		}
-	};
+  const getLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const lat = position.coords.latitude;
+        const long = position.coords.longitude;
+        const obj = {
+          lat: lat,
+          long: long,
+        };
+        searchLocation(obj, "latlong");
+      });
+    }
+  };
 
-	const searchLocation = async (query, type = "search") => {
-		let API_URL = `https://api.openweathermap.org/data/2.5/forecast?q=${query}&appid=${API_KEY}&units=metric&lang=tr`;
-		if (type === "latlong") {
-			API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${query.lat}&lon=${query.long}&appid=${API_KEY}&units=metric&lang=tr`;
-		}
-		try {
-			await axios.get(API_URL).then((response) => {
-				const dataTemp = [];
-				//Günü ayarlama
-				for (let i = 0; i < 6; i++) {
-					dataTemp.push(response.data.list[i * 1]);
-				}
+  const searchLocation = async (query, type = "search") => {
+    let API_URL = `https://api.openweathermap.org/data/2.5/forecast?q=${query}&appid=${API_KEY}&units=metric&lang=tr`;
+    if (type === "latlong") {
+      API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${query.lat}&lon=${query.long}&appid=${API_KEY}&units=metric&lang=tr`;
+    }
+    try {
+      await axios.get(API_URL).then((response) => {
+        const dataTemp = [];
+        //Günü ayarlama
+        for (let i = 0; i < 6; i++) {
+          dataTemp.push(response.data.list[i * 1]);
+        }
 
-				setWeatherId(response.data.list[0].weather[0].id);
+        setWeatherId(response.data.list[0].weather[0].id);
 
-				setLocationName(response.data.city.name);
-				setLocation(response.data.city.name);
-				setCurrentWeather(dataTemp);
-			});
-		} catch (error) {
-			console.error(error);
-		}
-	};
+        setLocationName(response.data.city.name);
+        setLocation(response.data.city.name);
+        setCurrentWeather(dataTemp);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-	return (
-		<div className="min-h-screen flex flex-col max-w-screen" data-theme="light">
-			<Background weatherId={weatherId} />
-			<div className="navbar z-10">
-				<div className="none gap-1">
-					<Link to="/images">
-						<button type="button" className="btn btn-ghost btn-circle avatar">
-							<div className="w-12 rounded-full">
-								<img
-									alt=""
-									src="https://i.pinimg.com/736x/02/32/ce/0232cec1a50de0b489b0591b64451204.jpg"
-								/>
-							</div>
-						</button>
-					</Link>
-					<h1 className="weather1">Weather App</h1>
+  return (
+    <div className="min-h-screen flex flex-col max-w-screen" data-theme="light">
+      <Background weatherId={weatherId} />
+      <div className="navbar z-10">
+        <div className="none gap-1">
+          <Link to="/images">
+            <button type="button" className="btn btn-ghost btn-circle avatar">
+              <div className="w-12 rounded-full">
+                <img
+                  alt=""
+                  src="https://i.pinimg.com/736x/02/32/ce/0232cec1a50de0b489b0591b64451204.jpg"
+                />
+              </div>
+            </button>
+          </Link>
+          <h1 className="weather1">Weather App</h1>
 
-					<div className="form-control">
-						<input
-							onClick={getLocation}
-							type="text"
-							placeholder="Enter location"
-							className="input input-bordered w-auto "
-							value={location}
-							onChange={(event) => setLocation(event.target.value)}
-							onKeyDownCapture={(event) => {
-								if (event.key === "Enter") {
-									searchLocation(location);
-								}
-							}}
-						/>
-					</div>
-				</div>
-			</div>
-			<div className="container-lg mx-auto p-2 flex justify-center items-center flex-col flex-grow w-full z-10">
-				<div className="bg-white/30 text-black p-4 rounded-lg shadow-md backdrop-blur-xl w-full sm:w-auto">
-          <h1 className="text-center text-5xl leading-relaxed text-black">{locationName}</h1>
+          <div className="form-control">
+            <input
+              onClick={getLocation}
+              type="text"
+              placeholder="Enter location"
+              className="input input-bordered w-auto "
+              value={location}
+              onChange={(event) => setLocation(event.target.value)}
+              onKeyDownCapture={(event) => {
+                if (event.key === "Enter") {
+                  searchLocation(location);
+                }
+              }}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="container-lg mx-auto p-2 flex justify-center items-center flex-col flex-grow w-full z-10">
+        <div className="bg-white/30 text-black p-4 rounded-lg shadow-md backdrop-blur-xl w-full sm:w-auto">
+          <h1 className="text-center text-5xl leading-relaxed text-black">
+            {locationName}
+          </h1>
           {currentWeather ? (
             <Card currentWeather={currentWeather} />
           ) : (
@@ -101,24 +103,28 @@ function HomePage() {
               </figure>
               <div className="card-body">
                 <h1 className="text-3xl">Veri bekleniyor!</h1>
-                <p>Hava durumunu görüntülemek için konum izninin açık olması ya da arama kutucuğundan istediğiniz şehri yazmanız gerekmektedir.</p>
+                <p>
+                  Hava durumunu görüntülemek için konum izninin açık olması ya
+                  da arama kutucuğundan istediğiniz şehri yazmanız
+                  gerekmektedir.
+                </p>
               </div>
             </div>
           )}
         </div>
-			</div>
-			<footer className="footer footer-center p-4 bg-base-300 text-base-content bottom-0 z-10">
-				<aside>
-					<p>-Made by Nil Ece Altuğ © 2024 -</p>
-				</aside>
-				<img
-					className="gif"
-					alt=""
-					src="https://cdn.dribbble.com/users/2120934/screenshots/6193512/11_rain.gif"
-				/>
-			</footer>
-		</div>
-	);
+      </div>
+      <footer className="footer footer-center p-4 bg-base-300 text-base-content bottom-0 z-10">
+        <aside>
+          <p>-Made by Nil Ece Altuğ © 2024 -</p>
+        </aside>
+        <img
+          className="gif"
+          alt=""
+          src="https://cdn.dribbble.com/users/2120934/screenshots/6193512/11_rain.gif"
+        />
+      </footer>
+    </div>
+  );
 }
 
 export default HomePage;
